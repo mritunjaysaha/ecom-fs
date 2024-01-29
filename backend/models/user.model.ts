c038b4c51651e4e1bdaa8db484bb8a2ad9acc8cf
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-29 23:22:54 +0530

    feat: added user model

---
+++ backend/models/user.model.ts
@@ -0,0 +1,71 @@
+import crypto from "crypto";
+import { Document, Model, Schema, model } from "mongoose";
+import { v1 as uuidV1 } from "uuid";
+
+export interface IUser extends Document {
+    firstName: string;
+    lastName: string;
+    email: string;
+    encryptedPassword: string;
+    salt: string;
+    orders: any;
+    discountCode: any;
+}
+
+const UserSchema: Schema = new Schema<IUser>(
+    {
+        firstName: {
+            type: String,
+            maxlength: 32,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            maxlength: 32,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        encryptedPassword: {
+            type: String,
+            required: true,
+        },
+        salt: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+UserSchema.virtual("password")
+    .set(function (password) {
+        this._password = password;
+        this.salt = uuidV1();
+        this.encryptedPassword = this.securePassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
+
+UserSchema.methods = {
+    authenticate: function (plainPassword: string) {
+        return this.securePassword(plainPassword) === this.encryptedPassword;
+    },
+    securePassword: function (plainPassword: string) {
+        if (!plainPassword) return "";
+
+        try {
+            return crypto
+                .createHmac("sha256", this.salt)
+                .update(plainPassword)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    },
+};
+
+export const UserModel: Model<IUser> = model<IUser>("users", UserSchema);
