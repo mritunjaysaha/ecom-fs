a41017a851b6c6f587ca405dfd7e4a918f378521
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-01-31 00:28:27 +0530

    feat: updated order schema

--- backend/models/order.model.ts
+++ backend/models/order.model.ts
@@ -5,11 +5,13 @@ import { IProduct } from "./product.model";
 export interface IOrder extends Document {
     products: IProduct[];
     discountCode: IDiscountCode;
+    transactionCompleted: boolean;
 }
 
 const OrderSchema: Schema = new Schema<IOrder>({
     products: [{ type: Schema.Types.ObjectId, ref: "ProductModel" }],
     discountCode: { type: Schema.Types.ObjectId, ref: "DiscountModel" },
+    transactionCompleted: { type: Boolean, default: false },
 });
 
 export const OrderModel: Model<IOrder> = model<IOrder>(
