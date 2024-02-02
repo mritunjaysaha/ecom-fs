5bb5293cab10621e02beceaec94edcc5f7a51154
mritunjaysaha <mritunjaysaha17@gmail.com>
2024-02-03 00:15:00 +0530

    feat: updated product card component to have increment and decrement  buttons

--- frontend/src/components/ProductCards.tsx
+++ frontend/src/components/ProductCards.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import { useSelector } from "../redux/store";
 import { Products } from "../types/Products";
 
 export const ProductCards: FC<Products> = ({ id, name, price }) => {
+    const { itemsQuantity } = useSelector((state) => state.app.cart);
+
+    const quantity = itemsQuantity[id];
+
     return (
         <div
             data-product-id={id}
@@ -9,12 +14,26 @@ export const ProductCards: FC<Products> = ({ id, name, price }) => {
         >
             <h3 className="text-2xl font-bold">{name}</h3>
             <p className="text-xl">${price}</p>
-            <button
-                data-add-cart
-                className="border rounded-full p-2 text-white"
-            >
-                Add to cart
-            </button>
+            {!quantity ? (
+                <button
+                    data-add-cart
+                    className="border rounded-full p-2 text-white"
+                >
+                    Add to cart
+                </button>
+            ) : (
+                <div className="flex items-center justify-between w-3/4 mx-auto mb-4">
+                    <button data-remove-from-cart className="text-4xl">
+                        {" "}
+                        -{" "}
+                    </button>
+                    <span className="text-xl font-semibold">{quantity}</span>
+                    <button data-add-cart className="text-4xl">
+                        {" "}
+                        +{" "}
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
